902f6ed524558aef5ee23b3d159b8fc38c7f58af
natchanonTo <natchanontomudbarung@gmail.com>
2024-08-20 18:38:36 +0700

    Update change.c

--- change.c
+++ change.c
@@ -37,7 +37,11 @@ void calculate(double num1 ,double num2 ,char operator){
 		result = num1*num2;
 		break;
 	case '/':
-		result = num1/num2;
+		if (num2 != 0){
+			result = num1/num2;
+		} else{
+			printf("Error");
+		}
 		break;
 	
 	default:
